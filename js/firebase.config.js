a686830bd833b275fb6b16a6a3a340a693bac244
Huy Nguyen <huynn911@gmail.com>
2026-04-19 11:07:57 +0700

    upd

---
+++ js/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASGit67F32vdr_Vp37qaomitfxmhJKcK4",
+    authDomain: "shop-valor.firebaseapp.com",
+    projectId: "shop-valor",
+    storageBucket: "shop-valor.firebasestorage.app",
+    messagingSenderId: "806884993481",
+    appId: "1:806884993481:web:7d83ec9ab5e58a65e72018",
+    measurementId: "G-14ML5088CZ",
+};
+
+firebase.initializeApp(firebaseConfig);
