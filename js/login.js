1a06cd3e99ba0bcf38cbab6024232fae653473ab
Huy Nguyen <huynn911@gmail.com>
2026-04-19 11:58:56 +0700

    upd

--- js/login.js
+++ js/login.js
@@ -1,5 +1,32 @@
 // chờ cho tới khi trang được tải xong hết thì mới thực hiện câu lệnh bên trong
-
+const btnGoogle = document.getElementById("btn-google");
+// lắng nghe sự kiện bấm vào nút đăng nhập bằng Google
+btnGoogle.addEventListener("click", () => {
+    // tạo một provider để đăng nhập với Google
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // đăng nhập với Google bằng popup
+    firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            var credential = result.credential;
+            var user = result.user; // sau khi đăng nhập thành công thì user sẽ là một object chứa thông tin của người dùng, ví dụ {displayName: "Nguyen Van A", email: "
+            console.log(user);
+            window.location.href = "/index.html"; // chuyển hướng về trang chủ sau khi đăng nhập thành công
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log(errorCode, errorMessage);
+            alert("Error", errorMessage);
+            // ...
+        });
+});
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.getElementById("login-form");
     console.log(loginForm);
--- js/render.js
+++ js/render.js
@@ -1,12 +1,16 @@
-// lây ra id của thẻ chứa các món ăn
-const foodContainer = document.getElementById("account-container");
-// lấy ra danh sách món ăn từ localStorage
-const foods = JSON.parse(localStorage.getItem("accounts")) || [];
-// duyệt qua từng món ăn và tạo thẻ HTML tương ứng
-let html = ``;
-
-foods.forEach((acc) => {
-    html += `
+const db = firebase.firestore();
+// lấy ra container để hiển thị sản phẩm
+const productContainer = document.getElementById("accounts-container");
+// lấy dữ liệu từ firestore và hiển thị ra giao diện
+let productHTML = ``; // biến này sẽ chứa HTML của tất cả sản phẩm, sau khi lấy dữ liệu từ firestore xong thì gán vào innerHTML của productContainer để hiển thị ra giao diện
+db.collection("products")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id);
+            let product = doc.data(); // doc.data() sẽ trả về dữ liệu của document dưới dạng object, ví dụ {name: "iPhone 14 Pro Max", price: 30000000, image: "https://..."}
+            console.log(product);
+            productHTML += `
         <a href="./html/detail-acc.html?${acc.id}" class="block w-full border border-indigo-600 bg-black rounded-xl overflow-hidden text-white">
                 <div class="px-5 py-2">
                     <img src="${acc.image}" />
@@ -19,14 +23,50 @@ foods.forEach((acc) => {
                     </div>
                     <div class="flex justify-between items-center">
                         <div class="text-xl">
-                            <div class=" text-indigo-900">${acc.sell}K</div>
+                            <div class=" text-indigo-900">${acc.sell}VNĐ</div>
                         </div>
                         <div href="" class="bg-indigo-950 block px-4 py-2 rounded-xl border border-indigo-600 overflow-hidden font-bold text-center">MUA NGAY</div>
                     </div>
                 </div>
             </a>
     `;
-});
+            productContainer.innerHTML = productHTML; // gán HTML của tất cả sản phẩm vào innerHTML của productContainer để hiển thị ra giao diện
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error); // nếu có lỗi xảy ra khi lấy dữ liệu từ firestore thì hiển thị lỗi ra console và alert cho người dùng biết
+        alert("Error getting documents: " + error.message);
+    });
 
-// chèn các thẻ HTML vào trong thẻ chứa món ăn
-foodContainer.innerHTML = html;
+// // lây ra id của thẻ chứa các món ăn
+// const foodContainer = document.getElementById("account-container");
+// // lấy ra danh sách món ăn từ localStorage
+// const foods = JSON.parse(localStorage.getItem("accounts")) || [];
+// // duyệt qua từng món ăn và tạo thẻ HTML tương ứng
+// let html = ``;
+
+// foods.forEach((acc) => {
+//     html += `
+//         <a href="./html/detail-acc.html?${acc.id}" class="block w-full border border-indigo-600 bg-black rounded-xl overflow-hidden text-white">
+//                 <div class="px-5 py-2">
+//                     <img src="${acc.image}" />
+//                     <p class="pt-3 pb-2 font-bold">${acc.name}</p>
+//                     <p class="">${acc.rank}</p>
+//                     <div class="space-y-1 py-2 text-gray-400">
+//                         <p>${acc.gun} Gun</p>
+//                         <p>${acc.bp} Battle Pass</p>
+//                         <p>${acc.mele} Mele</p>
+//                     </div>
+//                     <div class="flex justify-between items-center">
+//                         <div class="text-xl">
+//                             <div class=" text-indigo-900">${acc.sell}VNĐ</div>
+//                         </div>
+//                         <div href="" class="bg-indigo-950 block px-4 py-2 rounded-xl border border-indigo-600 overflow-hidden font-bold text-center">MUA NGAY</div>
+//                     </div>
+//                 </div>
+//             </a>
+//     `;
+// });
+
+// // chèn các thẻ HTML vào trong thẻ chứa món ăn
+// foodContainer.innerHTML = html;
